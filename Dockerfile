e24e6c8f0421f3d383ce2525223e2eefb0a8e478
reenu-dev <sroyreenu@gmail.com>
2026-09-06 10:57:21 +0000

    fixed

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,16 @@
 FROM ubuntu:latest
 
 RUN apt-get update && apt-get install -y \
-python3.10 \
-python3-pip \
-git
+    python3.10 \
+    python3-pip \
+    git \
+    && rm -rf /var/lib/apt/lists/*
 
 RUN pip3 install PyYAML
 
 COPY feed.py /usr/bin/feed.py
-
 COPY entrypoint.sh /entrypoint.sh
 
-ENTRYPOINY ["/entrypoint.sh"]
+RUN chmod +x /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+set -e
+
 echo "================="
 
-git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.name "${INPUT_NAME}"
 git config --global user.email "${INPUT_EMAIL}"
+
 git config --global --add safe.directory /github/workspace
 
 python3 /usr/bin/feed.py
 
-git add -A && git commit -m "Update Feed"
+git add -A
+
+git commit -m "Update Feed" || echo "No changes to commit"
 
 git push --set-upstream origin main
 
